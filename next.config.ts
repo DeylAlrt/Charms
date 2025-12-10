cbcb6d1bdbc7e54f744c0efaf5d07bce14abc463
Dale Alerta <alertadale@gmail.com>
2025-12-10 13:30:33 +0400

    Fixed next.config.ts with all settings

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,15 @@ const nextConfig: NextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'i.imgur.com',
+        pathname: '/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
